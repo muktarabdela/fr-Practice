5089f7dab904ed6c8084ed5e94a07756550f2c31
muktar <muktarabdela10@gmail.com>
2024-05-10 07:47:01 +0300

    finshed responsive  FAQ accordion

--- level-1/faq-accordion-main/react_project/src/component/Faq.jsx
+++ level-1/faq-accordion-main/react_project/src/component/Faq.jsx
@@ -3,69 +3,65 @@ import star from "../assets/images/icon-star.svg"
 import minus_icon from "../assets/images/icon-minus.svg"
 import plus_icon from "../assets/images//icon-plus.svg"
 const Faq = () => {
-    const [isOpen, setIsOpen] = useState(false)
-    const [clickId, setClickId] = useState("")
+    const [openAccordionId, setOpenAccordionId] = useState("")
 
-    const handleClick = () => {
-        if (isOpen === false) {
-            setIsOpen(true)
-        } else {
-            setIsOpen(false)
-        }
+    const handleClick = (accordionId) => {
+        setOpenAccordionId(openAccordionId === accordionId ? "" : accordionId)
     }
+
     return (
-        <div className='flex w-[33em] border-2  h-[30em] mx-auto relative bottom-40 rounded-md bg-white shadow-md'>
+        <div className='flex w-[20m] lg:w-[33em] border-2  lg:h-[30em] h-[40em] mx-4 lg:mx-auto relative bottom-40 rounded-md bg-white shadow-md'>
             <div>
-                <h1 className='text-4xl pl-10 pt-10 font-medium flex mt-10'>
+                <h1 className='text-4xl pl-10 pt-10 font-medium flex mt-4'>
                     <span className=''><img className=' w-7 pt-1 mr-5' src={star} alt="" /></span>
                     FAQs
                 </h1>
 
-                <div className='mt-10 ml-10'>
-                    <div onClick={handleClick}>
+                <div className='mt-10  lg:ml-10 ml-4'>
+                    <div onClick={() => handleClick("1")}>
                         <div
-                            className='text-[1.1em] font-medium mb-5 flex justify-between'>
+                            className='text-[1.1em] font-medium mb-5 flex justify-between cursor-pointer hover:underline'>
                             what i Frontend Mentor, and how will help me?
                             <span>
-                                <img className='w-5 mt-[5px] ml-[70px]' src={isOpen ? minus_icon : plus_icon} alt="" />
+                                <img className='w-5 mt-[5px] lg:mr-0 mr-[17px] lg:ml-[70px]' src={openAccordionId === "1" ? minus_icon : plus_icon} alt="" />
                             </span>
                         </div>
-                        <div id='1' className={` ${isOpen ? "text-md text-gray-400 w-[29em] mb-2" : "hidden"} text-md text-gray-400 w-[29em] mb-2`}>
+                        <div id='1' className={` ${openAccordionId === "1" ? "text-md text-gray-400 lg:w-[29em] w-[20em] mb-2" : "hidden"} `}>
                             Frontend mentor offers realistic coding challenges to help developers improve their fronted coding skills with project in html,css and javascript it's suitable for all levels and ideal for portfolio building
                         </div>
                     </div>
 
-                    <div onClick={handleClick}>
-                        <div className='text-[1.1em] font-medium mb-5 flex justify-between'>
+                    <div onClick={() => handleClick("2")}>
+                        <div className='text-[1.1em] font-medium mb-5 flex justify-between cursor-pointer hover:underline'>
                             is Frontend Mentor free?
                             <span>
-                                <img className='w-5 mt-[5px] ml-[20px] ' src={isOpen ? minus_icon : plus_icon} alt="" />
+                                <img className='w-5 mt-[5px] mr-[12px] lg:mr-0 ml-[20px] ' src={openAccordionId === "2" ? minus_icon : plus_icon} alt="" />
                             </span>
                         </div>
-                        <div id='2' className={` ${isOpen ? "text-md text-gray-400 w-[29em] mb-2" : "hidden"} text-md text-gray-400 w-[29em] mb-2`}>
+                        <div id='2' className={` ${openAccordionId === "2" ? "text-md text-gray-400 lg:w-[29em] w-[20em] mb-2 " : "hidden"}`}>
                             Frontend mentor offers realistic coding challenges to help developers improve their fronted coding skills with project in html,css and javascript it's suitable for all levels and ideal for portfolio building
                         </div>
                     </div>
 
-                    <div onClick={handleClick}>
-                        <div className='text-[1.1em] font-medium mb-5 flex justify-between'>
+                    <div onClick={() => handleClick("3")}>
+                        <div className='text-[1.1em] font-medium mb-5 flex justify-between cursor-pointer hover:underline'>
                             can i use Frontend mentor project im my portfolio?
                             <span>
-                                <img className='w-5 mt-[5px] ml-[20px]' src={isOpen ? minus_icon : plus_icon} alt="" />
+                                <img className='w-5 mt-[5px] lg:mr-0 mr-[20px] lg:ml-[20px]' src={openAccordionId === "3" ? minus_icon : plus_icon} alt="" />
                             </span>
                         </div>
-                        <div id='3' className={` ${isOpen ? "text-md text-gray-400 w-[29em] mb-2" : "hidden"} text-md text-gray-400 w-[29em] mb-2`}>
+                        <div id='3' className={` ${openAccordionId === "3" ? "text-md text-gray-400 lg:w-[29em] w-[20em] mb-2" : "hidden"} `}>
                             Frontend mentor offers realistic coding challenges to help developers improve their fronted coding skills with project in html,css and javascript it's suitable for all levels and ideal for portfolio building
                         </div>
                     </div>
 
-                    <div onClick={handleClick}>
-                        <div className='text-[1.1em] font-medium flex justify-between'>how can i get if i'm stuck on a challenge?
+                    <div onClick={() => handleClick("4")}>
+                        <div className='text-[1.1em] font-medium flex justify-between cursor-pointer hover:underline'>how can i get if i'm stuck on a challenge?
                             <span>
-                                <img className='w-5 mt-[5px] ml-[20px]' src={isOpen ? minus_icon : plus_icon} alt="" />
+                                <img className='w-5 mt-[5px] lg:mr-0 mr-[10px] lg:ml-[20px]' src={openAccordionId === "4" ? minus_icon : plus_icon} alt="" />
                             </span>
                         </div>
-                        <div id='4' className={` ${isOpen ? "text-md text-gray-400 w-[29em] mb-2" : "hidden"} text-md text-gray-400 w-[29em] mb-2`}>
+                        <div id='4' className={` ${openAccordionId === "4" ? "text-md text-gray-400 lg:w-[29em] w-[20em] mb-2" : "hidden"} `}>
                             Frontend mentor offers realistic coding challenges to help developers improve their fronted coding skills with project in html,css and javascript it's suitable for all levels and ideal for portfolio building
                         </div>
                     </div>
